829f362e874957ca80111f514b48f0154084d0ea
Dennis <dennisvanstrien2007@Gmail.com>
2026-05-15 13:52:18 +0200

    Added all files

---
+++ resources/views/Bear/index.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Bear</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js']) {{-- Dit is de link om Tailwind CSS en JavaScript te laden. Als je geen Javascript hoeft te gebruiker kan je de 'recrouces/js/app.js' er uit houden. --}}
+</head>
+<body>
+    <h1>Bear</h1>
+    <p> Dit is de index van de bear. Deze hebben GEEN relaties. </p>
+</body>
+</html>
